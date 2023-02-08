a9f818b64facc38cdd0c5ec496dd14db7cc16b61
Camilo Santander <camilosantander@usantotomas.edu.co>
2023-02-08 01:32:48 -0500

    Implemented the function to print out the question in the canvas

--- ui.py
+++ ui.py
@@ -1,8 +1,10 @@
 THEME_COLOR = "#375362"
 from tkinter import *
+from quiz_brain import QuizBrain
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz: QuizBrain):
+        self.quiz = quiz
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -11,6 +13,7 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
         self.text1 = self.canvas.create_text(150,
                                              125,
+                                             width=280,
                                              text="question",
                                              fill=THEME_COLOR,
                                              font=("Arial", 20, "italic"))
@@ -23,6 +26,10 @@ class QuizInterface:
         self.my_image2 = PhotoImage(file="./images/false.png")
         self.button2 = Button(image=self.my_image2, highlightthickness=0)
         self.button2.grid(row=2, column=1,)
-
-
+        self.get_next_question()
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.text1, text=q_text)
+
