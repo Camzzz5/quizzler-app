e51a55890322b871943abf71bb953c5199b5ae5e
Camilo Santander <camilosantander@usantotomas.edu.co>
2023-02-08 18:34:40 -0500

    adedd checking answer and score functionality

--- ui.py
+++ ui.py
@@ -21,15 +21,36 @@ class QuizInterface:
         self.label1 = Label(text="Score :0", fg="white", bg=THEME_COLOR)
         self.label1.grid(row=0, column=1)
         self.my_image1 = PhotoImage(file="./images/true.png")
-        self.button1 = Button(image=self.my_image1, highlightthickness=0)
+        self.button1 = Button(image=self.my_image1, highlightthickness=0, command=self.true_method)
         self.button1.grid(row=2, column=0)
         self.my_image2 = PhotoImage(file="./images/false.png")
-        self.button2 = Button(image=self.my_image2, highlightthickness=0)
+        self.button2 = Button(image=self.my_image2, highlightthickness=0,command=self.false_method)
         self.button2.grid(row=2, column=1,)
         self.get_next_question()
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.text1, text=q_text)
+        if self.quiz.still_has_questions():
+            self.canvas.config(bg="white")
+            self.label1.config(text=f"score:{self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.text1, text=q_text)
+        else:
+            self.canvas.config(bg="white")
+            self.canvas.itemconfig(self.text1, text="You have reached the end of the game")
+            self.button1.config(state="disabled")
+            self.button2.config(state="disabled")
+
+    def true_method(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_method(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        elif not is_right:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
 
